6dcc2360b4cc9e5f7218b2c624c3dfe46ee966cc
Krystian Hebel <krystian.hebel@3mdeb.com>
2024-04-12 16:50:13 +0200

    .github/workflows/build.yml: run 'make tests'
    This enables SHA tests to check if different compilers produce the same
    results.
    
    Definitions in string.h were moved outside of __STDC_HOSTED__ to avoid
    compiler warnings (promoted to errors because of -Werror) due to
    incompatible implicit declaration of built-in functions.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- include/string.h
+++ include/string.h
@@ -3,26 +3,31 @@
 
 #if __STDC_HOSTED__
 
-#include <string.h>	/* memcpy, memset */
+#include <string.h>	/* memcpy, memset, memcmp */
+
+/* Used in tests only */
+#define memcmp(l, r, n) __builtin_memcmp(l, r, n)
 
 #else
 
+/* Local declaration of bits of libc */
+
+void *memset(void *s, int c, size_t n);
+
+void *memcpy(void *dst, const void *src, size_t n);
+
+size_t strlen(const char *s);
+
+#endif /* __STDC_HOSTED__ */
+
 /*
- * Local declaration of bits of libc
- *
  * Use __builtin_???() wherever possible to allow the compiler to perform
  * optimisations (e.g. constant folding) where possible.  Calls to ???() will
  * be emitted as needed.
  */
 
-void *memset(void *s, int c, size_t n);
 #define memset(d, c, n) __builtin_memset(d, c, n)
-
-void *memcpy(void *dst, const void *src, size_t n);
 #define memcpy(d, s, n) __builtin_memcpy(d, s, n)
-
-size_t strlen(const char *s);
 #define strlen(s)       __builtin_strlen(s)
 
-#endif /* __STDC_HOSTED__ */
 #endif /* __STRINGS_H__ */
